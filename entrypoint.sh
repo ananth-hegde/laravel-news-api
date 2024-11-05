70bbed5585f4fb027c7a28cd29ed5f74c685827c
Ananth Hegde <ananth.dayakarananth.dayakar@gmail.com>
2024-11-05 17:03:39 +0530

    docker changes for auto init

---
+++ entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#wait for db up
+echo "Waiting for the database to be ready..."
+while ! nc -z $DB_HOST $DB_PORT; do
+  sleep 1
+done
+echo "Database is ready!"
+# Run database migrations
+php artisan migrate:fresh
+
+echo "Seeding the database..."
+
+# Fetch news articles
+php artisan app:fetch-news-articles
+
+echo "Database seeding completed!"
+# Execute the main container command
+exec "$@"
